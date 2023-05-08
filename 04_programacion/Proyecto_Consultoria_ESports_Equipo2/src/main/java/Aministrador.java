c794512506cb85325d74891a54beac542dd89dea
FranGS <eucaepiofrancisco.quezada@ikasle.egibide.org>
2023-05-08 21:00:26 +0200

    Creacion de clases

--- 04_programacion/Proyecto_Consultoria_ESports_Equipo2/src/main/java/Aministrador.java
+++ 04_programacion/Proyecto_Consultoria_ESports_Equipo2/src/main/java/Aministrador.java
@@ -1,45 +1,8 @@
-public class Aministrador {
+public class Aministrador extends Usuario{
 
-    private int id_admin;
-    private String contrasenya;
-    private String usuario;
 
-    public Aministrador(int id_admin, String contrasenya, String usuario) {
-        this.id_admin = id_admin;
-        this.contrasenya = contrasenya;
-        this.usuario = usuario;
+    public Aministrador(int id_usuario, String contrasenya, String usuario) {
+        super(id_usuario, contrasenya, usuario);
     }
 
-    public int getId_admin() {
-        return id_admin;
-    }
-
-    public void setId_admin(int id_admin) {
-        this.id_admin = id_admin;
-    }
-
-    public String getContrasenya() {
-        return contrasenya;
-    }
-
-    public void setContrasenya(String contrasenya) {
-        this.contrasenya = contrasenya;
-    }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    @Override
-    public String toString() {
-        return "Aministrador{" +
-                "id_admin=" + id_admin +
-                ", contrasenya='" + contrasenya + '\'' +
-                ", usuario='" + usuario + '\'' +
-                '}';
-    }
 }
---
+++ 04_programacion/Proyecto_Consultoria_ESports_Equipo2/src/main/java/Usuario.java
@@ -0,0 +1,48 @@
+public abstract class Usuario {
+    private int id_usuario;
+    private String contrasenya;
+    private String usuario;
+
+    public Usuario(int id_usuario, String contrasenya, String usuario) {
+        this.id_usuario = id_usuario;
+        this.contrasenya = contrasenya;
+        this.usuario = usuario;
+    }
+
+    public Usuario() {
+
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id_usuario=" + id_usuario +
+                ", contrasenya='" + contrasenya + '\'' +
+                ", usuario='" + usuario + '\'' +
+                '}';
+    }
+}

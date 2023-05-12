4e4e26ad2eec6a04b624bfb8b4b02c3fa37841aa
David <david.relloso@ikasle.egibide.org>
2023-05-12 19:58:49 +0200

    Creacion paquete gui

--- 04_programacion/Proyecto_Consultoria_ESports_Equipo2/src/main/java/Consultoria/pack/GUI/TablaClasificacionModel.java
+++ 04_programacion/Proyecto_Consultoria_ESports_Equipo2/src/main/java/Consultoria/pack/GUI/TablaClasificacionModel.java
@@ -4,21 +4,31 @@ import Consultoria.pack.Clases_Base.Equipo;
 import Consultoria.pack.Clases_Base.Partido;
 
 import javax.swing.table.AbstractTableModel;
+import java.util.List;
 
 public class TablaClasificacionModel extends AbstractTableModel {
 
-    private String[] columnas = {"Equipo", "ID", "Partidos ganados"};
+    private String[] columnasClasificacion = {"Equipo", "ID", "Partidos ganados"};
 
     private Equipo equipo;
+    private List<Partido> partidos;
+    private int num_ganados;
+
+    public TablaClasificacionModel() {
+    }
+
+    public TablaClasificacionModel(List<Partido> partidos) {
+        this.partidos = partidos;
+    }
 
     @Override
     public int getRowCount() {
-        return 0;
+        return partidos.size();
     }
 
     @Override
     public int getColumnCount() {
-        return columnas.length;
+        return columnasClasificacion.length;
     }
 
     @Override
@@ -29,9 +39,11 @@ public class TablaClasificacionModel extends AbstractTableModel {
             case 1:
                 return equipo.getId_equipo();
             case 2:
-                for (Partido partido:
-                     ) {
-
+                for (Partido partido: partidos) {
+                    if (partido.getEquipoGanador().equals(equipo.getId_equipo())) {
+                        num_ganados++;
+                    }
+                    return num_ganados;
                 }
         }
         return null;
@@ -39,6 +51,6 @@ public class TablaClasificacionModel extends AbstractTableModel {
 
     @Override
     public String getColumnName(int column) {
-        return columnas[column];
+        return columnasClasificacion[column];
     }
 }
--- 04_programacion/Proyecto_Consultoria_ESports_Equipo2/src/main/java/Consultoria/pack/GUI/verJornada.java
+++ 04_programacion/Proyecto_Consultoria_ESports_Equipo2/src/main/java/Consultoria/pack/GUI/verJornada.java
@@ -1,4 +1,28 @@
 package Consultoria.pack.GUI;
 
+import javax.swing.*;
+import javax.swing.table.TableModel;
+
 public class verJornada {
+    private JPanel panel1;
+    private JLabel label1;
+    private JTable table1;
+    private JScrollPane scrollpane1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Jornadas");
+        frame.setContentPane(new verJornada().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public verJornada() {
+
+        table1 = new JTable();
+        table1.setModel((TableModel) new TablaJornadaModel());
+        scrollpane1.setViewportView(table1);
+
+
+    }
 }

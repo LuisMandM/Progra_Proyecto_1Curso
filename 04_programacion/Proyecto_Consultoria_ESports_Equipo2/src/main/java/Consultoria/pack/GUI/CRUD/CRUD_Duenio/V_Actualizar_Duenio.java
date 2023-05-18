d66b81f92cf6a45cc05d95e2753937816cc0bcc5
FranGS <eucaepiofrancisco.quezada@ikasle.egibide.org>
2023-05-18 20:16:33 +0200

    commit clases.actualizadas.GUI

--- 04_programacion/Proyecto_Consultoria_ESports_Equipo2/src/main/java/Consultoria/pack/GUI/CRUD/CRUD_Duenio/V_Actualizar_Duenio.java
+++ 04_programacion/Proyecto_Consultoria_ESports_Equipo2/src/main/java/Consultoria/pack/GUI/CRUD/CRUD_Duenio/V_Actualizar_Duenio.java
@@ -3,9 +3,8 @@ package Consultoria.pack.GUI.CRUD.CRUD_Duenio;
 import javax.swing.*;
 
 public class V_Actualizar_Duenio {
-    private JComboBox comboBoxDuenio;
+    private JPanel panelActuDuenio;
+    private JLabel labelEscoDu;
+    private JComboBox comboBoxEscogerDueño;
     private JButton buttonSeleccionar;
-    private JLabel labelDuenio;
-    private JPanel panelActualizarDuenio;
-
 }
